65ad90cf755d4c0b96f8b74fc973e1c29a17312d
zuhdynadhif <zuhdy.nadhif.ayyasy@gmail.com>
2023-09-13 08:51:28 +0700

    UPDATE main/test.py

--- main/tests.py
+++ main/tests.py
@@ -2,6 +2,9 @@ from django.test import TestCase, Client
 
 # Create your tests here.
 
+# import Product pada models.py
+from .models import Product
+
 class mainTest(TestCase):
     def test_main_url_is_exist(self):
         response = Client().get('/main/')
@@ -9,4 +12,28 @@ class mainTest(TestCase):
 
     def test_main_using_main_template(self):
         response = Client().get('/main/')
-        self.assertTemplateUsed(response, 'main.html')
+        self.assertTemplateUsed(response, 'main/main.html')
+    
+    def test_attribute_field_type(self):
+        product = Product(
+            name = 'Kursi Gaming',
+            price = 2000000,
+            amount = 1,
+            description = 'Kursi Gaming tidak gratis ya'
+        )
+        self.assertEqual(type(product.name), str)
+        self.assertEqual(type(product.price), int)
+        self.assertEqual(type(product.amount), int)
+        self.assertEqual(type(product.description), str)
+    
+    def test_attribute_field_value(self):
+        product = Product(
+            name = 'Kursi Gaming',
+            price = 2000000,
+            amount = 1,
+            description = 'Kursi Gaming tidak gratis ya'
+        )
+        self.assertEqual(product.name, 'Kursi Gaming')
+        self.assertEqual(product.price, 2000000)
+        self.assertEqual(product.amount, 1)
+        self.assertEqual(product.description, 'Kursi Gaming tidak gratis ya')
